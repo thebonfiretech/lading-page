a20608c8c047cf74635d758e225799e30ea16ab8
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-29 02:54:15 +0000

    :lipstick: fix: Mudanças no css do componente miniGallery

--- src/components/miniGallery/styles.js
+++ src/components/miniGallery/styles.js
@@ -2,41 +2,40 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background: ${({ theme }) => theme.colors.background};
+  display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  display: flex;
   height: 820px;
   width: 100%;
-
 `;
 
 export const GalleryContainer = styled.div`
-  flex-direction: column; 
-  align-items: center;
-  gap: 18px;
   display: flex;
-  width:100%;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
   height: 70vh;
-`
+  gap: 18px;
+`;
 
 export const GalleryPhotos = styled.div`
   display: flex;
   gap: 18px;
   width: 80%;
-  
+
   @media (max-width: 820px) {
     display: none;
   }
 `;
 
 export const GalleryPhotos2 = styled.div`
-   flex-direction: row-reverse;
-   display: flex;
-   width: 80%;
-   gap: 18px;
+  display: flex;
+  flex-direction: row-reverse; 
+  width: 80%;
+  gap: 18px;
 
-@media (max-width: 820px) {
-  display: none;
-}
+  @media (max-width: 820px) {
+    display: none;
+  }
 `;
